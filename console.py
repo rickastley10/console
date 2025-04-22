dfe34fed9c500705d255f3a7383cade72ca8a2e6
rickastley10 <148710838+rickastley10@users.noreply.github.com>
2025-04-22 18:12:51 +0300

    Add files via upload

---
+++ console.py
@@ -0,0 +1,24 @@
+import time
+import os
+print(' launching console \n ### \n # # \n #  \n  ## \n ###')
+u = '$> '
+while 1 == 1:
+    ans = input(u)
+    if ans == 'mkdir':
+        file = input('$> (filename) > ')
+        os.mkdir(file)
+    if ans == 'exit':
+        exit()
+    if ans == 'fakehacker':
+        while 1 == 1:
+            print(u, 'sudo cmod load http://filezilla/82njfa/h347d/download')
+            print('main(hack)[for 39 goto 8932187966987]')
+            print('get{key}- getbrowser; browser.get password; browser.get_token(); ')
+            print('os.makefile(os.dll); os.store(os.dll, "token")')
+            print('for token make (+1)')
+            print('if {token} == 10:')
+            print('run (binary)')
+            print('1001010101010101010101010')
+            print('1001010101010')
+            print('010101010101010010101010101010101010101001010110')
+            
