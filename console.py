fcc070f2de8f9150f87a4fe696d4a6bb663b7ea7
rickastley10 <148710838+rickastley10@users.noreply.github.com>
2025-04-27 17:39:59 +0300

    console.py

--- console.py
+++ console.py
@@ -9,7 +9,7 @@ while 1 == 1:
         os.mkdir(file)
     if ans == 'exit':
         exit()
-    if ans == 'fakehacker':
+    if ans == 'fakehacker1':
         while 1 == 1:
             print(u, 'sudo cmod load http://filezilla/82njfa/h347d/download')
             print('main(hack)[for 39 goto 8932187966987]')
@@ -21,4 +21,5 @@ while 1 == 1:
             print('1001010101010101010101010')
             print('1001010101010')
             print('010101010101010010101010101010101010101001010110')
-            
+    if ans == 'fakehacker2':
+       print('launching console\n ###              \n # #              \n #                 \n ##              \n ###             \n $> sudo          \n $> mkdir         \n $> (filename) > ez                                                                                                      \n $> sudo make from appledough to just                                                                                    \n $> if (1 - x = 9) then create y                                                                                         \n $> [make y main()]                                                                                                      \n $> y create canvas                                                                                                      \n $> make canvas.screen(not'(3drender = vulcan)',  use'2dintegrated graphics' from imports>'https://applebee.net/repo/12384453             \n $> use from []{imports} == !error = 'null'                                                                              \n $> make x code = 0                                                                                                      \n $>')
